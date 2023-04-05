9c5daafd1ea05300f31e8e13a8b496635651b90b
lipanpan-hub <47287977+lipanpan-hub@users.noreply.github.com>
2023-04-05 23:05:53 +0800

    更新

--- src/components/Tips.tsx
+++ src/components/Tips.tsx
@@ -8,11 +8,13 @@ function Tips() {
 
   return (
     <div className="flex flex-col items-start justify-center h-full">
+
       <div className="flex flex-col items-start justify-center space-y-2">
         <div className="flex flex-row items-center space-x-2 pb-2">
           <BlubIcon className="w-8 h-8 text-gray-800" />
           <div className="text-xl font-medium text-gray-800">{i18n.t('common.tips') as string}</div>
         </div>
+
         <div className="text-gray-800 leading-7 flex flex-row">
           <RightTriangleIcon className="w-7 h-7 text-gray-800 shrink-0" strokeWidth="1.5" />
           <div>
@@ -21,8 +23,10 @@ function Tips() {
                 __html: i18n.t('tips.tips1') as string,
               }}
             />
+            <a href="https://syno.lipanpanx.com:65473/" target="_blank">获取KEY</a>
           </div>
         </div>
+
         <div className="text-gray-800 leading-7 flex flex-row">
           <RightTriangleIcon className="w-7 h-7 text-gray-800 shrink-0" strokeWidth="1.5" />
           <div
@@ -31,7 +35,8 @@ function Tips() {
             }}
           />
         </div>
-        <div className="text-gray-800 leading-7 flex flex-row">
+
+        {/* <div className="text-gray-800 leading-7 flex flex-row">
           <RightTriangleIcon className="w-7 h-7 text-gray-800 shrink-0" strokeWidth="1.5" />
           <div>
             <span className="border w-min inline py-0.5 px-2 border-gray-700 rounded-md">
@@ -47,7 +52,7 @@ function Tips() {
             </span>
             &nbsp; {i18n.t('tips.tips3b') as string}
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
